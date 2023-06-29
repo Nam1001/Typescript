8f81cbf69b217149c27b74751927c0782e5eac37
Dipanshu <dipanshusrivastava04@gmail.com>
2023-06-29 22:57:08 +0530

    types in typescript

---
+++ day3/index.ts
@@ -0,0 +1,16 @@
+var data=["anil","harsh","ram"]
+data.push("sam")   //this will allow me to push string value
+// data.push(100) 
+                   /*it will not allow me to do so because 
+                   i am pushing number in array and during declaration there is no number present inside the array
+                   therefore complier think that data array is collection of strings.
+                   */
+console.log(data)
+
+
+var arr=["hello","hii",10,true]
+
+arr.push(100)   /* now i can do both because during declaration both data types are present  */
+arr.push(false)
+
+var arr1:string[]=["hello","hii"] //this is a specific way to declaring string type of array
