99da34ad9dc767ffa1695022fa81a64e018b4018
Dipanshu <dipanshusrivastava04@gmail.com>
2023-06-29 22:58:29 +0530

    array in typescript

--- day3/index.js
+++ day3/index.js
@@ -6,7 +6,7 @@ i am pushing number in array and during declaration there is no number present i
 therefore complier think that data array is collection of strings.
 */
 console.log(data);
-var arr = ["hello", "hii", 10, true];
+var arr = ["hello", "hii", 10, true, 100];
 arr.push(100); /* now i can do both because during declaration both data types are present  */
 arr.push(false);
 var arr1 = ["hello", "hii"]; //this is a specific way to declaring string type of array
--- day3/index.ts
+++ day3/index.ts
@@ -5,10 +5,10 @@ data.push("sam")   //this will allow me to push string value
                    i am pushing number in array and during declaration there is no number present inside the array
                    therefore complier think that data array is collection of strings.
                    */
-console.log(data)
+console.log(data) 
 
 
-var arr=["hello","hii",10,true]
+var arr=["hello","hii",10,true,100]
 
 arr.push(100)   /* now i can do both because during declaration both data types are present  */
 arr.push(false)
