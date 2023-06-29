dcd3afe501d5b5d43870112f40dbf6c599d45f46
Dipanshu <dipanshusrivastava04@gmail.com>
2023-06-29 22:23:13 +0530

    day1

---
+++ day1/index.js
@@ -0,0 +1,4 @@
+var _a;
+var button = (_a = document.getElementById('btn')) === null || _a === void 0 ? void 0 : _a.addEventListener(('click'), function () {
+    console.log("hello");
+});
---
+++ day1/index.ts
@@ -0,0 +1,3 @@
+var button=document.getElementById('btn')?.addEventListener(('click'),()=>{
+    console.log("hello")
+})
